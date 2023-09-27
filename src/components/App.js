3f0222aa54b9e2b693226f63b70d5dd1e7d89ca3
Gregorio <Greg.del801@gmail.com>
2023-09-27 15:30:53 -0700

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
+const budget = 100
 
 function App() {
+  const [sushi, setSushi] = useState([])
+  const [min, setMin] = useState(0)
+  const [max, setMax] = useState(5)
+  const [money, setMoney] = useState(budget)
+
+  useEffect(() => {
+    fetch(API)
+    .then((res) => res.json())
+    .then((data) => setSushi(data))
+  }, [])
+
+  
+  function handleMoreClick() {
+    
+    setMin(min + 4)
+    setMax(max + 4)
+   
+  }
+  const tabledSushi = sushi.filter((s) => s.id > min && s.id < max)
+
+
+  function handleEatSushi(eatenSushi) {
+   if(eatenSushi.price <= money) {
+   setSushi(sushi.map(s => s === eatenSushi ? {...s, eaten: true} : s))
+   setMoney(money - eatenSushi.price)
+   
+    }
+
+  }
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer 
+      tabledSushi={tabledSushi}
+      onMoreClick={handleMoreClick}
+      onEatSushi={handleEatSushi}
+      />
+      <Table 
+      money={money}
+      plates={sushi.filter((s) => s.eaten)}
+      
+      />
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,24 @@
 import React from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi"
 
-function SushiContainer(props) {
+function SushiContainer({ tabledSushi, onEatSushi, onMoreClick}) {
+
+  const servedSushi = tabledSushi.map((s) => (
+    <Sushi
+    key={s.id}
+    sushi={s}
+    onEatSushi={onEatSushi}
+    />
+  ))
+
+  
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {servedSushi}
+      <MoreButton 
+      onMoreClick={onMoreClick}
+      />
     </div>
   );
 }
